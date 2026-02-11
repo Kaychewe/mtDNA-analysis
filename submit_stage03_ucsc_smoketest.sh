ca17d6b574df3c4390823f157adc3a576f7bb78a
Kaychewe <chewe.kasonde@outlook.com>
2026-02-10 19:14:14 -0500

    Added submit_stage03_ucsc_smoketest.sh

---
+++ submit_stage03_ucsc_smoketest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+# Submit UCSC tools bundle smoketest to local Cromwell.
+# Inputs:
+# - workflowSource: stage03_ucsc_smoketest.wdl
+# - workflowInputs: stage03_ucsc_smoketest.json
+
+ROOT_DIR="${PROJECT_ROOT:-$(cd "$(dirname "$0")" && pwd)}"
+WDL_PATH="${ROOT_DIR}/stage03_ucsc_smoketest.wdl"
+JSON_PATH="${ROOT_DIR}/stage03_ucsc_smoketest.json"
+
+curl -sS -X POST "http://localhost:8094/api/workflows/v1" \
+  -H "accept: application/json" \
+  -F workflowSource=@"$WDL_PATH" \
+  -F workflowInputs=@"$JSON_PATH"
